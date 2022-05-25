7db5f96300556354a4c92167bcbdceeacb21e32e
Romulo <romulo_conti@hotmail.com>
2022-05-24 23:44:38 -0300

    continuacao aula react

--- React/Projeto React/gamagit/src/App.js
+++ React/Projeto React/gamagit/src/App.js
@@ -1,17 +1,37 @@
- import React, { useState } from 'react';
- import logo from './logo.svg';
- import './App.css';
+import React from 'react';
+import Routes from './routes';
 
-// import React, { useState } from "react";
+import './style.css';
 
-function App(props) {
-  const [usuario, setUsuario ] = useState('');
+function App() {
   return (
-    <>
-    <input className="usuarioInput" placeholder="Usuário" onChange={} />
-    <button type="button">Pesquisar</button>
-    </>
+    <Routes />
   );
 }
 
 export default App;
+
+
+/*
+// PRIMEIRA AULA
+
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Routes from './routes';
+import React, { useState } from "react";
+
+function App(props) {
+  const [usuario, setUsuario ] = useState('');
+
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then (response => console.log(response));
+  }
+
+  return (
+    <>
+    <input className="usuarioInput" placeholder="Usuário" onChange={e => setUsuario(e.target.value)} />
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+    </>
+  );
+} */
